4d557b3d99ba3cff8c574a1311d19256e4cc120a
corebenediktus <64728148+corebenediktus@users.noreply.github.com>
2020-05-04 21:13:30 +0300

    Inc min CSS gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,7 @@
 const gulp = require ('gulp');
 const browserSync = require('browser-sync').create();
-
-
-
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
 
 // Static server
 gulp.task('browser-sync', function() {
@@ -12,4 +11,12 @@ gulp.task('browser-sync', function() {
       }
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+//min CSS files
+gulp.task('default', function () {
+    gulp.src('src/**/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist'));
 });
