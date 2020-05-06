47b535c27b44fcc8752cf9f1da155eb7f0d5980e
corebenediktus <64728148+corebenediktus@users.noreply.github.com>
2020-05-06 19:13:05 +0300

    feat: add gulp-sass in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,21 @@
-const gulp = require ('gulp');
+const {src, dest, watch} = require ('gulp');
 const browserSync = require('browser-sync').create();
 const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
+const sass = require('gulp-sass')
 
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
+    serveSass
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass) ;
+  watch("./js/*.js").on('change', browserSync.reload);
+};
 
 //min CSS files
 gulp.task('default', function () {
@@ -20,3 +24,13 @@ gulp.task('default', function () {
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('dist'));
 });
+
+// Compile sass into CSS & auto-inject into browsers
+function serveSass() {
+    return src("./sass/*.sass")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
