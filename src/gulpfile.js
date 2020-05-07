d0f32825e36911807af2cbc51b5fc6f9caf2fea5
corebenediktus <64728148+corebenediktus@users.noreply.github.com>
2020-05-07 19:47:20 +0300

    feat: dir SRC & DIST, replace files in SRC, inc fonts Futura Book & Demi and join + refact: add scss files to watch function

---
+++ src/gulpfile.js
@@ -0,0 +1,45 @@
+const {src, dest, watch} = require ('gulp');
+const browserSync = require('browser-sync').create();
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer'); 
+
+// Static server
+function bs() {
+    serveSass
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./sass/**/*.scss", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
+
+
+// Compile sass into CSS & auto-inject into browsers
+function serveSass() {
+    return src("./sass/**/*.sass", "./sass/**/*.scss")
+        .pipe(sass())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+
+// //min CSS files
+// function minCss() {
+//     src('src/**/*.css')
+//         .pipe(cssmin())
+//         .pipe(rename({suffix: '.min'}))
+//         .pipe(dest('dist'));
+// };
+
+
+
+exports.serve = bs;
